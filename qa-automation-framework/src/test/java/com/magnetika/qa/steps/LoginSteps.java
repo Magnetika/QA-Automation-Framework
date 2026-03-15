0e9c572d87faa61150dcc4646b433226cb766de4
Magnetika <agneslilla.szabo@gmail.com>
2026-03-15 14:43:26 +0100

    modify loginsteps.java

--- qa-automation-framework/src/test/java/com/magnetika/qa/steps/LoginSteps.java
+++ qa-automation-framework/src/test/java/com/magnetika/qa/steps/LoginSteps.java
@@ -2,25 +2,33 @@ package com.magnetika.qa.steps;
 
 import io.cucumber.java.en.*;
 import com.magnetika.qa.utils.DriverManager;
+import com.magnetika.qa.pages.LoginPage;
 
 public class LoginSteps {
 
-  @Given("user is on the login page")
-public void user_is_on_login_page() {
+    LoginPage loginPage;
 
-    DriverManager.initDriver();
-    DriverManager.driver.get("https://www.saucedemo.com/");
-}
+    @Given("user is on the login page")
+    public void user_is_on_login_page() {
+
+        DriverManager.initDriver();
+        DriverManager.driver.get("https://www.saucedemo.com/");
+
+        loginPage = new LoginPage(DriverManager.driver);
+    }
 
     @When("user enters valid username and password")
     public void user_enters_credentials() {
-        System.out.println("Entering credentials");
+
+        loginPage.login("standard_user", "secret_sauce");
     }
 
-  @Then("user should see the products page")
-public void user_should_see_products_page() {
+    @Then("user should see the products page")
+    public void user_should_see_products_page() {
 
-    DriverManager.quitDriver();
-}
+        String currentUrl = DriverManager.driver.getCurrentUrl();
+        System.out.println("Current URL: " + currentUrl);
 
+        DriverManager.quitDriver();
+    }
 }
