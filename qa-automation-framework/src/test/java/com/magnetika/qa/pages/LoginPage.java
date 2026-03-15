f3a381bd21015231de8d5af7d75b9098c7059dd5
Magnetika <agneslilla.szabo@gmail.com>
2026-03-15 15:17:45 +0100

    modify loginpage.java

--- qa-automation-framework/src/test/java/com/magnetika/qa/pages/LoginPage.java
+++ qa-automation-framework/src/test/java/com/magnetika/qa/pages/LoginPage.java
@@ -1,21 +1,16 @@
 package com.magnetika.qa.pages;
 
-import com.magnetika.qa.utils.WaitHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage {
-
-    WebDriver driver;
-    WaitHelper wait;
+public class LoginPage extends BasePage {
 
     By username = By.id("user-name");
     By password = By.id("password");
     By loginButton = By.id("login-button");
 
     public LoginPage(WebDriver driver){
-        this.driver = driver;
-        wait = new WaitHelper(driver);
+        super(driver);
     }
 
     public void login(String user, String pass){
